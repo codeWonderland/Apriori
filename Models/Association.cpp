23c7e3117950d2dff3046daecf4b3ca31cc1fb11
Alice Easter <alice.easter@mymail.champlain.edu>
2017-12-04 10:35:10 -0500

    Added Association

---
+++ Models/Association.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Alice Easter on 12/4/17.
+//
+
+#include "Association.h"
+
+Association::Association() : Itemset()
+{
+
+}
+
+Association::Association(std::string *mItems, int mSize, int mSupport) : Itemset(mItems, mSize), mSupport(mSupport)
+{
+
+}
+
+Association::~Association()
+{
+
+}
+
+int Association::getSupport()
+{
+	return this->mSupport;
+}
+
+void Association::setSupport(const int &support)
+{
+	this->mSupport = support;
+}
---
+++ Models/Association.h
@@ -0,0 +1,28 @@
+//
+// Created by Alice Easter on 12/4/17.
+//
+
+#ifndef FA17_CSI281_DECOSTA_EASTER_ASSOCIATION_H
+#define FA17_CSI281_DECOSTA_EASTER_ASSOCIATION_H
+
+#include "Itemset.h"
+
+class Association : public Itemset
+{
+	public:
+		Association();
+
+		Association(std::string *mItems, int mSize, int mSupport);
+
+		virtual ~Association();
+
+		int getSupport();
+
+		void setSupport(const int &support);
+
+	private:
+		int mSupport;
+};
+
+
+#endif //FA17_CSI281_DECOSTA_EASTER_ASSOCIATION_H
