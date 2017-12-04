24e8d5598a604f8dbbce8fcc69279f57d930d763
adam.decosta <me@adamdecosta.com>
2017-12-04 10:26:09 -0500

    Frquency

---
+++ Models/Frequency.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adam.decosta on 12/4/2017.
+//
+
+#include "Frequency.h"
---
+++ Models/Frequency.h
@@ -0,0 +1,26 @@
+//
+// Created by adam.decosta on 12/4/2017.
+//
+
+#ifndef FA17_CSI281_DECOSTA_EASTER_FREQUENCY_H
+#define FA17_CSI281_DECOSTA_EASTER_FREQUENCY_H
+
+#include <iostream>
+#include <string>
+
+#include "LinkedList.h"
+#include "Association.h"
+
+class Frequency {
+private:
+
+    std::string mID;
+    LinkedList<Association> mAssociations;
+
+public:
+
+
+};
+
+
+#endif //FA17_CSI281_DECOSTA_EASTER_FREQUENCY_H
