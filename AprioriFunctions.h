ff8a958ff9b3f5c94f0390cfc864c9cef117ffcf
adam.decosta <me@adamdecosta.com>
2017-12-06 18:57:54 -0500

    loadArray function created

--- AprioriFunctions.h
+++ AprioriFunctions.h
@@ -5,6 +5,13 @@
 #ifndef FA17_CSI281_DECOSTA_EASTER_APRIORIFUNCTIONS_H
 #define FA17_CSI281_DECOSTA_EASTER_APRIORIFUNCTIONS_H
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
+#include "Models/Itemset.h"
+
+void loadArray(Itemset *originalTransactions);
 
 #endif //FA17_CSI281_DECOSTA_EASTER_APRIORIFUNCTIONS_H
