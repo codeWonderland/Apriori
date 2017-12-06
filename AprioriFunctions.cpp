0caf90552a25b1b72785e029a56b4e8be3f1d95c
Alice Easter <alice.easter@mymail.champlain.edu>
2017-12-06 17:37:34 -0500

    Added AprioriFunctions files

---
+++ AprioriFunctions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alice Easter on 12/6/17.
+//
+
+#include "AprioriFunctions.h"
---
+++ AprioriFunctions.h
@@ -0,0 +1,15 @@
+//
+// Created by Alice Easter on 12/6/17.
+//
+
+#ifndef FA17_CSI281_DECOSTA_EASTER_APRIORIFUNCTIONS_H
+#define FA17_CSI281_DECOSTA_EASTER_APRIORIFUNCTIONS_H
+
+
+class AprioriFunctions
+{
+
+};
+
+
+#endif //FA17_CSI281_DECOSTA_EASTER_APRIORIFUNCTIONS_H
